0761f77dcaa27ee832a58210312b616a2e498df9
zemanue <manureco.97@gmail.com>
2025-11-09 14:04:25 +0100

    Update job descriptions and titles in jobs.js
    Revised job titles and descriptions for clarity, consistency, and to reflect past and current responsibilities more accurately. Updated language to better distinguish between current and previous roles, and expanded on administrative and teaching duties.

--- src/constants/jobs.js
+++ src/constants/jobs.js
@@ -1,11 +1,11 @@
 export const jobs = [
     {
         id: 4,
-        title: "Profesor de programación de software",
+        title: "Profesor de programación y administrador de academia",
         dateStart: "jun. 2025",
         dateEnd: "actualidad",
         place: "Logiscool (Marbella)",
-        description: `Talleres digitales bilingües (inglés y español) a niños/as y adolescentes. \n Enseñanza de conceptos básicos de programación con enseñanza visual por bloques (tipo Scratch), código y/o mixta. \n Introducción a Python y desarrollo de videjuegos con Unity. \n Colaboración con centros educativos para actividades extraescolares: English International College (EIC) y British International School of Marbella (BISM). \n Administración de la academia: uso de CRM, comunicación con familias y clientes potenciales, etc.`
+        description: `Enseño programación básica a niños/as en inglés y español, utilizando enseñanza visual por bloques (tipo Scratch) a través de la plataforma Scoolcode. \n Enseño Python y desarrollo de videojuegos con Unity a adolescentes. \n Colaboro con centros educativos para actividades extraescolares: English International College (EIC) y British International School of Marbella (BISM). \n Gestiono la administración de la academia: uso de CRM, facturación, comunicación con familias y clientes potenciales, etc.`
     },
     {
         id: 3,
@@ -13,7 +13,7 @@ export const jobs = [
         dateStart: "ago. 2025",
         dateEnd: "sept. 2025",
         place: "Mainjobs (Marbella)",
-        description: `Enseñanza de competencias digitales a niños/as y adolescentes. \n Programa estatal gratuito y a través de proyectos gamificados.`
+        description: `Enseñé competencias digitales a niños/as y adolescentes. \n Programa estatal gratuito y a través de proyectos gamificados.`
     },
     {
         id: 2,
@@ -21,7 +21,7 @@ export const jobs = [
         dateStart: "mar. 2025",
         dateEnd: "jun. 2025",
         place: "Bimfloat, Málaga (Prácticas DAM)",
-        description: `Desarrollo de novelas visuales infantiles con Unity y la extensión Naninovel. \n Configuración de prefabs, escenas, scripts en C#, etc. \n Uso de IA Generativa para historias e imágenes(ChatGPT, Gemini, extensión Stable Difussion en Krita).`
+        description: `Desarrollé novelas visuales infantiles con Unity y la extensión Naninovel. \n Me encargué de configurar prefabs, escenas, scripts en C#, etc. \n Investigué sobre IA Generativa para la creación de historias e imágenes (ChatGPT, Gemini, Krita AI Diffusion, etc.).`
     },
     {
         id: 1,
@@ -29,6 +29,6 @@ export const jobs = [
         dateStart: "sept. 2015",
         dateEnd: "jun. 2025",
         place: "Autónomo",
-        description: `Ed. Primaria y ESO: Matemáticas, Lengua, Inglés...\n Experiencia con necesidades educativas: discapacidad visual, dislexia, discalculia, TDAH... \n Creación y uso de materiales manipulativos y digitales, metodologias activas, etc. \n Clases de español a alumnado extranjero.`
+        description: `He dado clases particulares a estudiantes de Ed. Primaria y ESO: Matemáticas, Lengua, Inglés...\n Adquirí experiencia frente a diferentes necesidades educativas: discapacidad visual, dislexia, discalculia, TDAH... \n Creé y usé materiales manipulativos y digitales, empleando siempre metodologías activas. \n Enseñé español a alumnado extranjero.`
     }
 ];
